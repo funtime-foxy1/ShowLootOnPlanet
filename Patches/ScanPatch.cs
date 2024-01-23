ac03d160c975a620c8663b23593c691ce1693980
FunFoxRR <dldilley01@outlook.com>
2024-01-23 18:22:26 -0500

    first commit

---
+++ Patches/ScanPatch.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using HarmonyLib;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Object = UnityEngine.Object;
+
+namespace ShowLootOnPlanet.Patches
+{
+    [HarmonyPatch]
+    internal class ScanPatch
+    {
+        static GameObject counter;
+        static TextMeshProUGUI textMesh;
+        private static float displayLeft;
+        private const float DisplayTime = 2f;
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(HUDManager), "PingScan_performed")]
+        private static void OnScan(HUDManager __instance, InputAction.CallbackContext context)
+        {
+            if (GameNetworkManager.Instance.localPlayerController == null)
+                return;
+            if (GameNetworkManager.Instance.localPlayerController.isInHangarShipRoom)
+                return;
+
+            PlanetModBase.log.LogInfo("SCAN!");
+            if (!counter)
+                CopyValueCounter();
+
+            System.Random random = new System.Random(StartOfRound.Instance.randomMapSeed + 91);
+			int objectAmount = 0;
+			int TotalValue = 0;
+			int num4 = 0;
+			GrabbableObject[] array = UnityEngine.Object.FindObjectsOfType<GrabbableObject>();
+			for (int num5 = 0; num5 < array.Length; num5++)
+			{
+				if (array[num5].itemProperties.isScrap && !array[num5].isInShipRoom && !array[num5].isInElevator)
+				{
+					num4 += array[num5].itemProperties.maxValue - array[num5].itemProperties.minValue;
+					TotalValue += Mathf.Clamp(random.Next(array[num5].itemProperties.minValue, array[num5].itemProperties.maxValue), array[num5].scrapValue - 6 * num5, array[num5].scrapValue + 9 * num5);
+					objectAmount++;
+				}
+			}
+			PlanetModBase.log.LogInfo("Amount of objects: " + objectAmount + " || Value of all: " + TotalValue);
+
+			if (objectAmount > 0) {
+                textMesh.text = $"MOON: ${TotalValue:F0} (${objectAmount})";
+                displayLeft = DisplayTime;
+                if (!counter.activeSelf)
+                    GameNetworkManager.Instance.StartCoroutine(PlanetLoot());
+            }
+		}
+
+        private static IEnumerator PlanetLoot()
+        {
+            counter.SetActive(true);
+            while (displayLeft > 0f)
+            {
+                float time = displayLeft;
+                displayLeft = 0f;
+                yield return new WaitForSeconds(time);
+            }
+            counter.SetActive(false);
+        }
+
+        /// <summary>
+        /// Copy an existing object loaded by the game for the display of ship loot and put it in the right position.
+        /// </summary>
+        private static void CopyValueCounter()
+        {
+            GameObject _counter = GameObject.Find("/Systems/UI/Canvas/IngamePlayerHUD/BottomMiddle/ValueCounter");
+            if (!counter)
+                PlanetModBase.log.LogWarning("Failed to find ValueCounter object to copy!");
+            counter = Object.Instantiate(_counter.gameObject, _counter.transform.parent, false);
+            counter.transform.Translate(0f, 1f, 0f);
+            Vector3 pos = counter.transform.localPosition;
+            counter.transform.localPosition = new Vector3(pos.x + 50f, -50f, pos.z);
+            textMesh = counter.GetComponentInChildren<TextMeshProUGUI>();
+            PlanetModBase.log.LogInfo("Child1: " + counter.GetComponentsInChildren<Transform>()[1]);
+            PlanetModBase.log.LogInfo("Child2: " + counter.GetComponentsInChildren<Transform>()[2]);
+
+            switch (PlanetModBase.colorValue)
+            {
+                case ColorType.White:
+                    textMesh.color = new Color(0.9056604f, 0.9056604f, 0.9056604f);
+                    break;
+                case ColorType.Red:
+                    textMesh.color = new Color(1, 0.3820755f, 0.3820755f);
+                    break;
+                case ColorType.Orange:
+                    textMesh.color = new Color(1, 0.7653924f, 0.3803922f);
+                    break;
+                case ColorType.Yellow:
+                    textMesh.color = new Color(0.9899307f, 1, 0.3803922f);
+                    break;
+                case ColorType.Green:
+                    textMesh.color = new Color(0.3803922f, 1, 0.6797408f);
+                    break;
+                case ColorType.Blue:
+                    textMesh.color = new Color(0.3803922f, 0.8848869f, 1);
+                    break;
+                case ColorType.Purple:
+                    textMesh.color = new Color(0.6813213f, 0.3803922f, 1);
+                    break;
+                case ColorType.Pink:
+                    textMesh.color = new Color(0.9750692f, 0.3803922f, 1);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,71 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using ShowLootOnPlanet.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LethalConfig;
+using LethalConfig.ConfigItems;
+using LethalConfig.ConfigItems.Options;
+using Color = UnityEngine.Color;
+
+namespace ShowLootOnPlanet
+{
+    public enum ColorType
+    {
+        White,
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        Purple,
+        Pink
+    }
+    [BepInDependency("ainavt.lc.lethalconfig")]
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class PlanetModBase : BaseUnityPlugin
+    {
+        private const string GUID = "funfoxrr.PlanetLoot";
+        private const string NAME = "Show Planet Loot";
+        private const string VERSION = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(GUID);
+
+        public static PlanetModBase Instance;
+
+        internal static ManualLogSource log;
+
+        public static BepInEx.Configuration.ConfigEntry<ColorType> color;
+        public static ColorType colorValue;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            log = BepInEx.Logging.Logger.CreateLogSource(GUID);
+
+            log.LogInfo("The mod has awakened!");
+
+            color = Config.Bind("General", "Color", ColorType.Green, "Change the color of the text!");
+            LethalConfigManager.AddConfigItem(new EnumDropDownConfigItem<ColorType>(color, new EnumDropDownOptions {}));
+            color.Value = colorValue;
+
+            color.SettingChanged += Color_SettingChanged;
+
+            harmony.PatchAll(typeof(PlanetModBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+            harmony.PatchAll(typeof(ScanPatch));
+        }
+
+        private void Color_SettingChanged(object sender, EventArgs e)
+        {
+            colorValue = color.Value;
+        }
+    }
+}
